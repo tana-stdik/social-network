9d7e09582102950be1fcd7782641b309cbed5e7a
tama-stdik <tana-stdik@gmail.com>
2022-11-24 16:33:45 +0100

    lesson 19 react-router

---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import c from './Music.module.css';
+
+const Music = (props) => {
+  return (
+    <div>
+      Music
+    </div>
+  );
+}
+
+export default Music;
---
+++ src/components/Setting/Setting.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import c from './Setting.module.css';
+
+const Setting = (props) => {
+  return (
+    <div>
+      Setting
+    </div>
+  );
+}
+
+export default Setting;
